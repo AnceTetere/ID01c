41ddad05b65f2fe7d24b6e22eb43c481b44be2ab
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-09-10 10:41:41 +0300

    Update 4_ID01c_templateSplit_NAS_AS.R

--- 4_ID01c_templateSplit_NAS_AS.R
+++ 4_ID01c_templateSplit_NAS_AS.R
@@ -1,23 +1,6 @@
-path <- file.path(base_path, paste0(year, "Q", Q))
-
-#1 Ielādē šablonu
-#template_path <- file.path(path, "izstrade", "intermediate_tables", "starting_templates")
-#load(file.path(template_path, paste0("template_ID01c_2000_", year, "Q", Q, ".RData")))
-
-#s <- get(paste0("template_ID01c_2000_", year, "Q", Q))
-#rm(list = paste0("template_ID01c_2000_", year, "Q", Q))
-
-#2 Sadali NAS un AS apakštabulās
+#1 Sadali NAS un AS apakštabulās
 X$S <- factor(X$S)
 
 X_split <- split(X, X$S)
 list2env(X_split, envir = .GlobalEnv)
 rm(X, X_split)
-
-#3 Saglabā apakštabulas
-intermediate_path <- file.path(path, "izstrade", "intermediate_tables")
-#save(NAS, file = file.path(intermediate_path, "NAS.RData"))
-#rm(NAS)
-
-#save(AS, file = file.path(intermediate_path, "AS.RData"))
-#rm(AS)
