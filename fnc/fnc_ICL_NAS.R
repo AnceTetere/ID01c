09767a52f76787e7b3e28083358e63177000cbef
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-09-10 16:57:54 +0300

    Update fnc_ICL_NAS.R

--- fnc/fnc_ICL_NAS.R
+++ fnc/fnc_ICL_NAS.R
@@ -1,21 +1,19 @@
-ICL_NAS <- function(year, Q) {
+ICL_NSA <- function(year, Q, i) {
 
-options(digits = 22)
-#1. Ielādē non-adjusted IXs no ICL_yyyy_aprekini.
-library(XLConnect)
+#1 Ielādē non-adjusted indeksus no ICL_yyyy_aprekini.xlsx.
 wb <- loadWorkbook(paste0("../1_sagatavosana/ICL_", year, "_aprekini.xlsx"),
                    create = FALSE,
-                   password = NULL)
+                  password = NULL)
 ws <- readWorksheet(wb, sheet = "all_transpose") 
-rm(wb)
-detach("package:XLConnect", unload = TRUE)
-
 ws <- ws[ , 1:5]
 colnames(ws) <- ws[1, ]
 colnames(ws)[4] <- "indicator"
+rm(wb)
+detach("package:XLConnect", unload = TRUE)
+
 colnames(ws)[5] <- "ICL_value_NAS"
 ws <- ws[-1, ]
-rownames(ws) <- NULL 
+rownames(ws) <- NULL
 
 #2 saglabā datus
 assign("NAS_data", ws)
