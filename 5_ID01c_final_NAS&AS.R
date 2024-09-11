4ea743658529aef217c0b58ae52d24824e7db069
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-09-11 14:42:28 +0300

    Update 5_ID01c_final_NAS&AS.R

--- 5_ID01c_final_NAS&AS.R
+++ 5_ID01c_final_NAS&AS.R
@@ -1,3 +1,4 @@
+#----------------Izstrādā NAS un AS
 c <- c("NAS", "AS")
 IID <- data.frame()
 
@@ -12,28 +13,27 @@ n <- ICL_dati(year, Q, i)
 #PĀRBAUDE VAI JAU ATKAL NEESI AIZMIRSUSI TRANSPONĒT
 a <- paste0("ICL_value_", i)
 if (sum(n[ , a][n$Gads == year] < 100, na.rm = TRUE) > 0) {
-  stop("Failos 'aprēķini_yyyy.xlsx' vai 'ICL_yyyyCq_AS.xls' pirms Macros palaišanas esi aizmirsusi transponēt datus!")
+  stop("Failos 'aprēķini_yyyy.xlsx' vai 'ICL_ggggCq_AS.xls' pirms Macros palaišanas esi aizmirsusi transponēt datus!")
 }
 
 #3 Noformē
-#  pārmaini indikatoru
-n$ii[n$ii == "Total"] <- "LC_TOTAL"
-n$ii[n$ii == "Wages"] <- "LC_WAG_TOT"
-n$ii[n$ii == "Other"] <- "LC_OTH"
+n$ii[n$ii == "tt"] <- "CL_T"
+n$ii[n$ii == "ww"] <- "CL_WT"
+n$ii[n$ii == "oo"] <- "CL_O"
 
 #  izveido time_period
 n$time_period <- paste0(n$Gads, "Q", n$Q)
 n$Gads <- NULL
 n$Q <- NULL
-n <- n[ , c("time_period", "Nace", "ii", a)]
+n <- n[ , c("time_period", "n", "ii", a)]
 
 #  izveido savienošanaa agregātu
-n$forMerge <- paste0(n$time_period, n$ii, "_", n$Nace)
-t$forMerge <- paste0(t$T, t$I, "_", t$NACE)
+n$forMerge <- paste0(n$time_period, n$ii, "_", n$n)
+t$forMerge <- paste0(t$TIME, t$INDICATOR, "_", t$N)
 t_order <- t$forMerge
 
 if (any(c(length(t$forMerge[!(t$forMerge %in% n$forMerge)]), length(n$forMerge[!(t$forMerge %in% n$forMerge)])) != 0)) {
-  stop("5_IID010c_final_NAS&AS: Savienojamo vērtību nesakritība.") 
+  stop("5_ID01c_final_NAS&AS: Savienojamo vērtību nesakritība.")
 }
 
 #4 Savieno
@@ -44,14 +44,25 @@ mergedDF$I2020 <- mergedDF[ , a]
 mergedDF <- mergedDF[order(match(mergedDF$forMerge, t_order)), ]
 mergedDF <- mergedDF[ , ailes_order]
 rownames(mergedDF) <- NULL
-rm(n, t, t_order)
+#rm(n, t, t_order)
 
 #5 Pēdējās pārbaudes un noglabā
 if (sum(mergedDF$S == i) == nrow(mergedDF)) {
-  IID <- rbind(IID, mergedDF)
+  gD <- mergedDF #gD: kā gatavs dati
   rm(mergedDF)
 } else {
-  stop("5_IID010c_final_NAS&AS: S ailē norāde neatbilst.")
+  stop("5_ID01c_final_NAS&AS: S ailē norāde neatbilst.")
 }
+
+#6 Sarēķini procentus
+gP <- percentChange(year, Q, gD, i) #gP: kā gatavs procenti
+IID <- rbind(IID, gP)
+rm(gP, gD)
 }
-rm(c, i)
+
+if (nrow(IID) == length(row_order)) {
+  IID <- IID[order(match(IID$rindas, row_order)),]
+  rownames(IID) <- NULL
+} else {
+  stop("5_ID01c_final_NAS&AS: Gala nenoformētajā tabulā rindas neatbilst! \n")
+}
