1be96228a834024fc1e276f49ef93962ba790e38
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-11 18:06:05 +0200

    Delete 3_ID01c_finalTemplate.R

--- 3_ID01c_finalTemplate.R
+++
@@ -1,8 +0,0 @@
-#--------------- SAVIENO ŠABLONUS-----
-#1 Savieno iepriekšējā ceturkšņa DB failu ar tekošo ceturksni un izformē.
-X <- rbind(Temp, t) # 11 172
-rm(Temp, t)
-
-X$I2020 <- ""
-X$I2020_X <- ""
-X$PCH_SAME <- ""
