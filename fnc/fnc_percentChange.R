8332db91ec377615574ccd18258b8b1a139521cf
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-11 18:36:10 +0200

    Update fnc_percentChange.R

--- fnc/fnc_percentChange.R
+++ fnc/fnc_percentChange.R
@@ -1,56 +1,53 @@
+percentChange <- function(gD, i) {
 
-#Procentu sarēķins
-
-percentChange <- function(year, Q, gD, i) {
-
-#pārbauda tabulu
-if (sum(gD$S == i) == nrow(gD)) {
+#pārbaudi tabulu
+if (sum(gD$SESON == i) == nrow(gD)) {
   
-  burti <- gD$N[gD$T == paste0(year, "Q", Q) & gD$I == "CL_T"]
+  burti <- gD$N[gD$TIME == paste0(year, "Q", Q) & gD$INDICATOR == "TOTAL"]
   gadi <- c(year:2001)
   
-  #1 Sadala pa ii
-  g_split <- split(gD, gD$I)
+  #1 Sadala pa indicator
+  g_split <- split(gD, gD$INDICATOR)
   names(g_split) <- paste0("gD_", names(g_split))
   g_ind <-
     names(g_split) 
-  list2env(g_split, envir = .GlobalEnv)
+  list2env(g_split, envir = environment())
   rm(gD, g_split)
   
-  #2 Aprēķina procentus 
   for (gads in gadi) {
     for (g in g_ind) {
       x <- get(g)
       for (burts in burti) {
         if (gads == year) {
-          #vispirms sarēķina tekošo gadu
           for (q in Q:1) {
-            x$PCH_SAME[x$T == paste0(year, "Q", q) & x$N == burts] <-
-              x$I2020[x$T == paste0(year, "Q", q) &
-                        x$N == burts] / x$I2020[x$T == paste0(year - 1, "Q", q) &
+            x$PCH_SAME[x$TIME == paste0(year, "Q", q) & x$N == burts] <-
+              x$I2020[x$TIME == paste0(year, "Q", q) &
+                        x$N == burts] / x$I2020[x$TIME == paste0(year - 1, "Q", q) &
                                                      x$N == burts] * 100 - 100
           }
         } else {
           #tad pārējo
           for (q in 4:1) {
-            x$PCH_SAME[x$T == paste0(gads, "Q", q) & x$N == burts] <-
-              x$I2020[x$T == paste0(gads, "Q", q) &
-                        x$N == burts] / x$I2020[x$T == paste0(gads - 1, "Q", q) &
+            x$PCH_SAME[x$TIME == paste0(gads, "Q", q) & x$N == burts] <-
+              x$I2020[x$TIME == paste0(gads, "Q", q) &
+                        x$N == burts] / x$I2020[x$TIME == paste0(gads - 1, "Q", q) &
                                                      x$N == burts] * 100 - 100
           }
         }
       }
-      assign(g, x)
+      assign(g, x, , envir = environment())
       rm(x)
     }
   }
   rm(burts, g, gadi, gads, q)
   
   #3 Savieno un atdod atpakaļ
-  s <- rbind(gD_CL_O, gD_CL_T, gD_CL_WT)
-  rm(gD_CL_O, gD_CL_T, gD_CL_WT)
+  #fileName <- paste0("gataviProc_", c[i])
+  s <- rbind(gD_OTH, gD_TOTAL, gD_WAG_TOT)
   
-  rm(burti, g_ind)
+  #save(list = fileName,
+       #file = file.path(intermediate_path, paste0(fileName, "00Q1_", substr(year, 3, 4), "Q", Q, ".RData")))
+  rm(burti, g_ind, gD_OTH, gD_TOTAL, gD_WAG_TOT)
 } else {
   print("Datu koriģēšanas atzīmes nesakrīt.")
 }
