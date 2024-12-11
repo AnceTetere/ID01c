f63801b73020d023f68d4bdb7f870fd011b3bf1c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-11 18:32:32 +0200

    Update fnc_ICL_NAS.R

--- fnc/fnc_ICL_NAS.R
+++ fnc/fnc_ICL_NAS.R
@@ -1,23 +1,27 @@
-ICL_NSA <- function(year, Q, i) {
+IC_NS <- function(i) {
 
-#1 Ielādē non-adjusted indeksus no ICL_yyyy_aprekini.xlsx.
-wb <- loadWorkbook(paste0("../1_sagatavosana/ICL_", year, "_aprekini.xlsx"),
+#1 Ielādē non-adjusted IC_yyyy_aprekini.xlsx.
+  
+if (!file.exists(paste0("../IC/ID_ESTAT/", year, "/IC", year, "_Q", Q, "/workflow/1_Excel/1_sagatavosana/IC_", year, "_aprekini.xlsm"))) {
+  stop("Jo funkcija IC_dati neatrada .RDATA failus mapē 'data', \
+        PROGRAMMA BLOĶĒJAS.")}
+  
+wb <- loadWorkbook(paste0("../IC/ID_ESTAT/", year, "/IC", year, "_Q", Q, "/workflow/1_Excel/1_sagatavosana/IC_", year, "_aprekini.xlsm"),
                    create = FALSE,
-                  password = NULL)
-ws <- readWorksheet(wb, sheet = "all_transpose") 
+                   password = NULL)
+ws <- readWorksheet(wb, sheet = "all_transpose")
 ws <- ws[ , 1:5]
 colnames(ws) <- ws[1, ]
 colnames(ws)[4] <- "indicator"
 rm(wb)
-detach("package:XLConnect", unload = TRUE)
 
-colnames(ws)[5] <- "ICL_value_NAS"
+colnames(ws)[5] <- "IC_value_NS"
 ws <- ws[-1, ]
 rownames(ws) <- NULL
 
-#2 saglabā datus
-assign("NAS_data", ws)
+#2 Nosaukuma maiņa
+assign("NS_data", ws)
 rm(ws)
 
-return(NAS_data)
+return(NS_data)
 }
