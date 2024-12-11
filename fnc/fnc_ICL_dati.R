11346b4c2dd8788f748f511f8f4e5d41e2c8ce76
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-11 18:23:23 +0200

    Update fnc_ICL_dati.R

--- fnc/fnc_ICL_dati.R
+++ fnc/fnc_ICL_dati.R
@@ -1,20 +1,26 @@
-ICL_dati <- function(year, Q, i) {
+IC_dati <- function(i) {
   
-  #1. Ielādē indeksus.
-  nD <- paste0(tolower(i), "ICL_00Q1_", substr(year, 3,4), "Q", Q)
-  if (file.exists(paste0(ICL_path, nD, ".RData"))) {
-    load(paste0(ICL_path, nD, ".RData"))
-  } else {
-    x <- ICL_NAS(year, Q, i)
-    assign(nD, x)
-  }
+  #1 Ielādē indeksus.
+  nD <- paste0(tolower(i), "IC_00Q1_", substr(year, 3,4), "Q", Q)
+  
+  #Ja programmatūra neatradīs .RDATA failus mapē 'data', 
+  #funkcija IC_NS(i) centīsies to izstrādāt no ESTAT sagataves datnēm.
+  #Tā bloķēsies ja fails netiks atrasts.
+  
+  if (file.exists(paste0("data/", nD, ".RData"))) {
+    load(paste0("data/", nD, ".RData"))
+  } else if(i == "NS") {
+      assign(nD, IC_NS(i), envir = environment())
+    } else {
+      stop("fnc_IC_dati: Izstrādā funkciju IC_SS(year, Q, i) līdzīgu IC_NS(year, Q, i), kas paņem SS datus no Excel dokumenta.")}
+  
   df <- get(nD)
-  rm(nD)  
+  rm(list = nD, nD)  
   
-  colnames(df)[5] <- paste0("ICL_value_", i)
-  df$Nace[df$Nace == "B_S LASP"] <- "B-S"  
-  df$Nace[df$Nace == "B_S_LASP"] <- "B-S"
-  rownames(df) <- NULL #7980
+  colnames(df)[5] <- paste0("IC_value_", i)
+  df$N[df$N == "B_S LASP"] <- "B-S" 
+  df$N[df$N == "B_S_LASP"] <- "B-S"
+  rownames(df) <- NULL
     
-    return(df)
+  return(df)
 }
